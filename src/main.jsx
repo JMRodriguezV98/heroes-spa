c29384e83aeb989e95d61fe214bd42fb82110779
JMRodriguezV98 <mriciosama@gmail.com>
2024-03-04 18:00:50 -0500

    comentario extra

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import { HeroesApp } from './HeroesApp'
 import './styles.css'
+
+//Leer el ultimo recurso de solucion a las imagenes en produccion
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
