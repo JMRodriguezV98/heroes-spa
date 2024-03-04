373acbb2a55166198d2869d1d748f1b61ecf9ed1
JMRodriguezV98 <mriciosama@gmail.com>
2024-03-04 17:50:00 -0500

    rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { HeroesRoutes } from "../heroes"
 import { LoginPages } from "../auth"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 
 export const AppRouter = () => {
@@ -9,15 +10,20 @@ export const AppRouter = () => {
     <>
       <Routes>
 
-        <Route path="login" element={ <LoginPages /> } />
+        {/* <Route path="login" element={ <LoginPages /> } /> */}
+        <Route path="/login" element={ 
+          <PublicRoute>
+            <LoginPages />
+          </PublicRoute>
+        }/>
         
+        {/* <Route path="/*" element={ <HeroesRoutes /> } /> */}
         <Route path="/*" element={ 
           <PrivateRoute>
             <HeroesRoutes />
           </PrivateRoute>
         }/>
 
-        {/* <Route path="/*" element={ <HeroesRoutes /> } /> */}
 
       </Routes>
     </>
